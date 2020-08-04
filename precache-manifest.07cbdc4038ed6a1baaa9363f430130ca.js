92d2f4ba9c5a8d1e58138b2a0503a25cc20c90f5
LaMalanga <rebeccareds@gmail.com>
2020-08-04 12:45:26 -0700

    Updates

---
+++ precache-manifest.07cbdc4038ed6a1baaa9363f430130ca.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a485b7dac183f62c841cc0caefa03312",
+    "url": "/accountownerclient/index.html"
+  },
+  {
+    "revision": "92569af572872bd72e01",
+    "url": "/accountownerclient/static/css/10.37c8f0ee.chunk.css"
+  },
+  {
+    "revision": "5d3388819432ed393d67",
+    "url": "/accountownerclient/static/css/11.1d9a2034.chunk.css"
+  },
+  {
+    "revision": "854d153f21e29529f7c1",
+    "url": "/accountownerclient/static/css/12.1d9a2034.chunk.css"
+  },
+  {
+    "revision": "701f19f39f4ed90da608",
+    "url": "/accountownerclient/static/css/2.21d3525d.chunk.css"
+  },
+  {
+    "revision": "606aa3fe4fb364b178bf",
+    "url": "/accountownerclient/static/css/6.9a72b8c0.chunk.css"
+  },
+  {
+    "revision": "897446457acde381513b",
+    "url": "/accountownerclient/static/css/7.37c8f0ee.chunk.css"
+  },
+  {
+    "revision": "cf7008e0427ab5aa59a5",
+    "url": "/accountownerclient/static/css/8.37c8f0ee.chunk.css"
+  },
+  {
+    "revision": "d44a47720f7dfeba86e0",
+    "url": "/accountownerclient/static/css/9.37c8f0ee.chunk.css"
+  },
+  {
+    "revision": "1701ca20e8df004051db",
+    "url": "/accountownerclient/static/css/main.8390ce8f.chunk.css"
+  },
+  {
+    "revision": "a1f6f0d13db2611e8679",
+    "url": "/accountownerclient/static/js/0.fd40fc5f.chunk.js"
+  },
+  {
+    "revision": "49b3ddd6ad9c4ba368c7",
+    "url": "/accountownerclient/static/js/1.be21c520.chunk.js"
+  },
+  {
+    "revision": "92569af572872bd72e01",
+    "url": "/accountownerclient/static/js/10.ca8dbba0.chunk.js"
+  },
+  {
+    "revision": "5d3388819432ed393d67",
+    "url": "/accountownerclient/static/js/11.acbcd87a.chunk.js"
+  },
+  {
+    "revision": "854d153f21e29529f7c1",
+    "url": "/accountownerclient/static/js/12.9f559e57.chunk.js"
+  },
+  {
+    "revision": "9f7523ec9dc9dac37140",
+    "url": "/accountownerclient/static/js/13.93615751.chunk.js"
+  },
+  {
+    "revision": "3b879968644fbd0a6d4b",
+    "url": "/accountownerclient/static/js/14.745e9979.chunk.js"
+  },
+  {
+    "revision": "701f19f39f4ed90da608",
+    "url": "/accountownerclient/static/js/2.cc5384fd.chunk.js"
+  },
+  {
+    "revision": "000d6c36287224a7ff48f88e64636454",
+    "url": "/accountownerclient/static/js/2.cc5384fd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7e69aa05ddf7a4336f24",
+    "url": "/accountownerclient/static/js/3.a8d29266.chunk.js"
+  },
+  {
+    "revision": "362cd232e6b400756e3bc1723069a8cd",
+    "url": "/accountownerclient/static/js/3.a8d29266.chunk.js.LICENSE"
+  },
+  {
+    "revision": "606aa3fe4fb364b178bf",
+    "url": "/accountownerclient/static/js/6.84912728.chunk.js"
+  },
+  {
+    "revision": "e89ad3dfb38ac973fcf2df05f27946de",
+    "url": "/accountownerclient/static/js/6.84912728.chunk.js.LICENSE"
+  },
+  {
+    "revision": "897446457acde381513b",
+    "url": "/accountownerclient/static/js/7.19dc231d.chunk.js"
+  },
+  {
+    "revision": "cf7008e0427ab5aa59a5",
+    "url": "/accountownerclient/static/js/8.19d9b80f.chunk.js"
+  },
+  {
+    "revision": "d44a47720f7dfeba86e0",
+    "url": "/accountownerclient/static/js/9.f21baf86.chunk.js"
+  },
+  {
+    "revision": "1701ca20e8df004051db",
+    "url": "/accountownerclient/static/js/main.02f5d5c1.chunk.js"
+  },
+  {
+    "revision": "fea24fdc61aaee293f1d",
+    "url": "/accountownerclient/static/js/runtime-main.e6bdac32.js"
+  },
+  {
+    "revision": "8c20481d522a1bf8c654d2fcb5d769b2",
+    "url": "/accountownerclient/static/media/bribe.8c20481d.svg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/accountownerclient/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/accountownerclient/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/accountownerclient/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/accountownerclient/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/accountownerclient/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
